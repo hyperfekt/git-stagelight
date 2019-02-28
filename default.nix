d062312a6e65d84dc8de127cd8415e9e8f2769da
hyperfekt <git@hyperfekt.net>
2019-02-28 22:31:40 +0100

    add nix derivation

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ stdenv, makeWrapper, git, coreutils, ncurses, gdb, gawk }:
+stdenv.mkDerivation {
+  pname = "git-stagelight";
+  version = "1.0.1";
+  src = ./.;
+  dontBuild = true;
+  buildInputs = [ makeWrapper ];
+  installPhase = ''
+    cp pre-commit $out
+    wrapProgram $out \
+      --prefix PATH : "${stdenv.lib.makeBinPath [ git coreutils ncurses gdb gawk ]}"
+  '';
+  meta = with stdenv.lib; {
+    description = "git hook that prompts for new && unstaged files whenever you commit";
+    homepage = "https://github.com/hyperfekt/git-stagelight";
+    license = licenses.mpl20;
+  };
+}
